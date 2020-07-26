d7c226e19df336073905297305f771054539a1d7
ChenHuang <alan.tolearn@gmail.com>
2020-07-26 12:04:06 -0400

    Change frequency and duration to remove ads

--- javascripts/blocker.js
+++ javascripts/blocker.js
@@ -2,6 +2,8 @@
  * Block ads every 30 seconds for 30 minutes
  */
 function blocker() {
+  const intervalTime = 5 * 1000
+  const duration = 3 * 60 * 60 * 1000
   let interval = null
   let timeout = null
   return function blockAds () {
@@ -11,10 +13,10 @@ function blocker() {
       const adIFrames = [...document.querySelectorAll('iframe:not([id])')]
       // Move ads away from screen
       adIFrames.forEach((iframe) => (iframe.style.bottom = '-10000px'))
-    }, 1000 * 15)
+    }, intervalTime)
     timeout = setTimeout(() => {
       clearInterval(interval)
-    }, 1000 * 60 * 30)
+    }, duration)
   }
 }
 
